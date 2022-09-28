2d676f774c79d389cd25e546e67600b840b026b6
DabasishDasJoy <dabasishdasjoy92@gmail.com>
2022-09-29 02:08:13 +0600

    details/side bar first component completed

--- src/components/ActivityDetails/ActivityDetails.js
+++ src/components/ActivityDetails/ActivityDetails.js
@@ -3,7 +3,8 @@ import img from "../../images/dabasish.jpg";
 const ActivityDetails = () => {
   return (
     <div className="p-5">
-      <div>
+      {/* First component container */}
+      <div className="flex flex-col gap-3">
         {/* Name & Location */}
         <div className="flex">
           <img src={img} alt="" className="rounded-2xl w-1/6" />
@@ -14,7 +15,7 @@ const ActivityDetails = () => {
         </div>
 
         {/* height, weight  */}
-        <div className="bg-slate-100 rounded-md flex justify-around">
+        <div className="bg-slate-100 rounded-md flex justify-around py-2">
           <div className="text-center">
             <h1 className="font-bold text-2xl">
               75<span className="text-xs text-gray-400">Kg</span>
@@ -35,8 +36,28 @@ const ActivityDetails = () => {
           </div>
         </div>
 
-        <div></div>
+        <div>
+          <h1>Add A Break</h1>
+          <div className="bg-slate-100 py-5 px-2 rounded-xl flex justify-around">
+            <button className="rounded-full bg-white p-2 font-medium">
+              20s
+            </button>
+            <button className="rounded-full bg-white p-2 font-medium">
+              10s
+            </button>
+            <button className="rounded-full bg-white p-2 font-medium">
+              30s
+            </button>
+            <button className="rounded-full bg-white p-2 font-medium">
+              40s
+            </button>
+            <button className="rounded-full bg-white p-2 font-medium">
+              50s
+            </button>
+          </div>
+        </div>
       </div>
+      {/* first component end */}
       <div></div>
       <div></div>
     </div>
