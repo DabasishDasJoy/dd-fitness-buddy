335828ae6bb0eab285c5033d1a7e3e2ab0b5c89a
DabasishDasJoy <dabasishdasjoy92@gmail.com>
2022-09-28 21:17:46 +0600

    icon and favicon added

--- src/App.js
+++ src/App.js
@@ -1,15 +1,7 @@
-import { ShoppingCart } from "react-hero-icon/solid";
 import "./App.css";
 
 function App() {
-  return (
-    <div>
-      <h1>
-        <ShoppingCart />
-        Hellow world
-      </h1>
-    </div>
-  );
+  return <div></div>;
 }
 
 export default App;
